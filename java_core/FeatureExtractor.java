36b203d30b78a05378b118d46925418b17835c86
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-23 11:04:02 +0530

    converting flow to feature array

--- java_core/FeatureExtractor.java
+++ java_core/FeatureExtractor.java
@@ -1,3 +1,16 @@
 import java.util.*;
 
+public class FeatureExtractor {
+
+    public static double[] extractFeatures(Map<String, String> flow) {
+        double duration = Double.parseDouble(flow.get("duration"));
+        double total_pkts = Double.parseDouble(flow.get("total_pkts"));
+        double total_bytes = Double.parseDouble(flow.get("total_bytes"));
+        double mean_pkt_len = Double.parseDouble(flow.get("mean_pkt_len"));
+        double pkt_rate = Double.parseDouble(flow.get("pkt_rate"));
+        int protocol = Integer.parseInt(flow.get("protocol")); 
+
+        return new double[]{duration, total_pkts, total_bytes, mean_pkt_len, pkt_rate, protocol};
+    }
+}
 
