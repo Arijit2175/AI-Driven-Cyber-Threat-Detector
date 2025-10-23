a9347d03cdc359f2963b570963d7868ecc65880a
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-23 11:18:04 +0530

    for logging alerts

--- java_core/AlertLogger.java
+++ java_core/AlertLogger.java
@@ -1,3 +1,21 @@
 import java.io.*;
 import java.time.LocalDateTime;
 
+public class AlertLogger {
+
+    private String logFile;
+
+    public AlertLogger(String logFile) {
+        this.logFile = logFile;
+    }
+
+    public void logAlert(String message) {
+        try (FileWriter fw = new FileWriter(logFile, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(LocalDateTime.now() + " - ALERT: " + message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
