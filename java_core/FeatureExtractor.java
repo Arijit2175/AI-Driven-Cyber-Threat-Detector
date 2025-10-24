2dc09d818a672a96b228bda01d96c9f94a20bf02
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-24 14:41:37 +0530

    made a change to protocol types

--- java_core/FeatureExtractor.java
+++ java_core/FeatureExtractor.java
@@ -8,8 +8,21 @@ public class FeatureExtractor {
         double total_bytes = Double.parseDouble(flow.get("total_bytes"));
         double mean_pkt_len = Double.parseDouble(flow.get("mean_pkt_len"));
         double pkt_rate = Double.parseDouble(flow.get("pkt_rate"));
-        int protocol = Integer.parseInt(flow.get("protocol")); 
-
+        String proto = flow.get("protocol").toUpperCase();
+        int protocol;
+        switch (proto) {
+            case "TCP":
+                protocol = 6;
+                break;
+            case "UDP":
+                protocol = 17;
+                break;
+            case "ICMP":
+                protocol = 1;
+                break;
+            default:
+                protocol = 0; 
+        }
         return new double[]{duration, total_pkts, total_bytes, mean_pkt_len, pkt_rate, protocol};
     }
 }
