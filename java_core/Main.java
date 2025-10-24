37dcdb696ee846867057e7dddf2ceddf77d62fe3
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-24 14:55:05 +0530

    made changes for file path

--- java_core/Main.java
+++ java_core/Main.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -6,26 +7,29 @@ public class Main {
 
         String csvFile = "../datasets/sample_traffic.csv";
         String serverUrl = "http://127.0.0.1:5000/predict";
-        String logFile = "alerts.log";
+
+        String logFile = System.getProperty("user.dir") + File.separator + ".." + File.separator + "logs" + File.separator + "alerts.log";
+        AlertLogger logger = new AlertLogger(logFile);
 
         PacketCapture capture = new PacketCapture(csvFile);
         ThreatDetector detector = new ThreatDetector(serverUrl);
-        AlertLogger logger = new AlertLogger(logFile);
+
+        logger.logAlert("=== New detection session started ===");
 
         List<Map<String, String>> flows = capture.readFlows();
 
         for (Map<String, String> flowMap : flows) {
-    double[] features = FeatureExtractor.extractFeatures(flowMap);
-    int prediction = detector.predict(features);
+            double[] features = FeatureExtractor.extractFeatures(flowMap);
+            int prediction = detector.predict(features);
 
-    if (prediction == 1) {
-        logger.logAlert("Malicious flow detected: " + Arrays.toString(features));
-        System.out.println("ALERT! Malicious flow: " + Arrays.toString(features));
-    } else {
-        logger.logAlert("Normal flow: " + Arrays.toString(features)); 
-        System.out.println("Normal flow: " + Arrays.toString(features));
-    }
-}
+            if (prediction == 1) {
+                logger.logAlert("Malicious flow detected: " + Arrays.toString(features));
+                System.out.println("ALERT! Malicious flow: " + Arrays.toString(features));
+            } else {
+                logger.logAlert("Normal flow: " + Arrays.toString(features));
+                System.out.println("Normal flow: " + Arrays.toString(features));
+            }
+        }
 
         System.out.println("Detection complete. Alerts logged to " + logFile);
     }
