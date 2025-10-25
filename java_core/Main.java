1029dffc8059685f4415b806a667ff7a07e9dbd2
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-25 13:49:50 +0530

    made a change near alert logging

--- java_core/Main.java
+++ java_core/Main.java
@@ -1,4 +1,4 @@
-import java.io.*;
+import java.io.File;
 import java.util.*;
 
 public class Main {
@@ -13,17 +13,10 @@ public class Main {
         if (!logsFolder.exists()) logsFolder.mkdirs();
 
         String logFilePath = logsFolderPath + File.separator + "alerts.log";
-        AlertLogger logger = new AlertLogger(logFilePath);
-        logger.logAlert("=== New detection session started ===");
-
         String maliciousCsvPath = logsFolderPath + File.separator + "malicious_flows.csv";
-        File maliciousCsv = new File(maliciousCsvPath);
-        boolean headerWritten = maliciousCsv.exists();
-        if (!headerWritten) {
-            try (PrintWriter pw = new PrintWriter(new FileWriter(maliciousCsv, true))) {
-                pw.println("duration,total_pkts,total_bytes,mean_pkt_len,pkt_rate,protocol");
-            }
-        }
+
+        AlertLogger logger = new AlertLogger(logFilePath, maliciousCsvPath);
+        logger.logAlert("=== New detection session started ===");
 
         PacketCapture capture = new PacketCapture(csvFile);
         ThreatDetector detector = new ThreatDetector(serverUrl);
@@ -35,14 +28,10 @@ public class Main {
             int prediction = detector.predict(features);
 
             if (prediction == 1) {
-                logger.logAlert("Malicious flow detected: " + Arrays.toString(features));
+                logger.logMalicious(features); 
                 System.out.println("ALERT! Malicious flow: " + Arrays.toString(features));
-
-                try (PrintWriter pw = new PrintWriter(new FileWriter(maliciousCsv, true))) {
-                    pw.println(features[0] + "," + features[1] + "," + features[2] + "," +
-                               features[3] + "," + features[4] + "," + features[5]);
-                }
             } else {
+                logger.logAlert("Normal flow: " + Arrays.toString(features));
                 System.out.println("Normal flow: " + Arrays.toString(features));
             }
         }
