9e9bfc88b2fb0ce55111f62d0a900f22c42bc41d
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-25 13:52:09 +0530

    made a change

--- java_core/AlertLogger.java
+++ java_core/AlertLogger.java
@@ -1,35 +1,15 @@
 import java.io.*;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 
 public class AlertLogger {
 
     private String logFile;
-    private String maliciousCsvFile;
-    private boolean writeMaliciousCsv;
 
     public AlertLogger(String logFile) {
         this.logFile = logFile;
-        this.writeMaliciousCsv = false;
-    }
-
-    public AlertLogger(String logFile, String maliciousCsvFile) {
-        this.logFile = logFile;
-        this.maliciousCsvFile = maliciousCsvFile;
-        this.writeMaliciousCsv = true;
-
-        File csvFile = new File(maliciousCsvFile);
-        File parentDir = csvFile.getParentFile();
+        File parentDir = new File(logFile).getParentFile();
         if (!parentDir.exists()) {
             parentDir.mkdirs();
-        }
-
-        if (!csvFile.exists()) {
-            try (PrintWriter pw = new PrintWriter(new FileWriter(csvFile, true))) {
-                pw.println("duration,total_pkts,total_bytes,mean_pkt_len,pkt_rate,protocol");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
@@ -41,25 +21,5 @@ public class AlertLogger {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void logMaliciousFlow(double[] features) {
-        if (!writeMaliciousCsv) return;
-
-        try (PrintWriter pw = new PrintWriter(new FileWriter(maliciousCsvFile, true))) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < features.length; i++) {
-                sb.append(features[i]);
-                if (i < features.length - 1) sb.append(",");
-            }
-            pw.println(sb.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void logMalicious(double[] features) {
-        logAlert("Malicious flow detected: " + Arrays.toString(features));
-        logMaliciousFlow(features);
     }
 }
